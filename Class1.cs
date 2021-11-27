1f3d684bd36420886b4afc262951692d94beaa38
Hazard (Cyprian Skrzypczak) <42750285+Hazardu@users.noreply.github.com>
2021-11-27 23:32:31 +0100

    Sync settings

--- Class1.cs
+++ Class1.cs
@@ -1,53 +1,171 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
+using System.IO;
 using TheForest.Utils;
 using UnityEngine;
 
-namespace TheFlash
+namespace TokyoDrift
 {
-    class Class1 : FirstPersonCharacter
+    public static class ModSettings
     {
-     
+        public static float bounciness = 0.9f, friction = 0, acceleration = 15, bouncetimer = 3, turnRate = 1f;
+        public static void SendSyncEvent()
+        {
+            using (MemoryStream stream = new MemoryStream(sizeof(int) + sizeof(float) * 5))
+            {
+                using (BinaryWriter w = new BinaryWriter(stream))
+                {
+                    w.Write(1);
+                    w.Write(bounciness);
+                    w.Write(acceleration);
+                    w.Write(bouncetimer);
+                    w.Write(friction);
+                    w.Write(turnRate);
+                }
+                NetworkManager.SendCommand(stream.ToArray(), Bolt.GlobalTargets.Everyone);
+            }
+        }
+    }
+
+
+    public class ShellModOptionsGUI : MonoBehaviour
+    {
+        static ShellModOptionsGUI instance;
+        [ModAPI.Attributes.ExecuteOnGameStart]
+
+        static void Init()
+        {
+            instance = new GameObject().AddComponent<ShellModOptionsGUI>();
+        }
+        void Start()
+        {
+            Reset();
+        }
+        public static void Reset()
+        {
+            instance.textf = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                instance.textf[i] = "";
+            }
+        }
+        bool _show = false;
+        void Update()
+        {
+            if (ModAPI.Input.GetButtonDown("Menu"))
+            {
+                _show = !_show;
+                if (LocalPlayer.FpCharacter)
+                {
+
+                if (_show)
+                    LocalPlayer.FpCharacter.LockView(true);
+                else
+                    LocalPlayer.FpCharacter.UnLockView();
+                }
+            }
+        }
+        string[] textf = new string[5];
+        void DrawTF(ref float val, int i)
+        {
+            if (textf[i] == "")
+                textf[i] = "" + val;
+            textf[i] = GUILayout.TextField(textf[i]);
+            if (float.TryParse(textf[i], out float f))
+            {
+                val = f;
+            }
+        }
+        void DrawSlider(ref float val, float min, float max, int i)
+        {
+            float f = GUILayout.HorizontalSlider(val, min, max);
+            if (val <= max && val != f)
+            {
+                val = f;
+                textf[i] = "" + f;
+            }
+        }
+        void OnGUI()
+        {
+            if (!_show) return;
+            int i = 0;
+            GUILayout.BeginVertical(GUI.skin.box);
+            GUILayout.Label("friction: (default 0.45 or 0.15 for snow area) = " + ModSettings.friction.ToString("N2"));
+            DrawTF(ref ModSettings.friction, i);
+            DrawSlider(ref ModSettings.friction, 0f, 1f, i);
+            i++;
+            GUILayout.Space(15f);
+
+            GUILayout.Label("bounciness: (default 0.9)" + ModSettings.bounciness.ToString("N2"));
+            DrawTF(ref ModSettings.bounciness, i);
+            DrawSlider(ref ModSettings.bounciness, 0f, 1f, i);
+            i++;
+            GUILayout.Space(15f);
+
+            GUILayout.Label("shift key acceleration: (default 0) " + ModSettings.acceleration.ToString("N2"));
+            DrawTF(ref ModSettings.acceleration, i);
+            DrawSlider(ref ModSettings.acceleration, 0f, 100f, i);
+            i++;
+            GUILayout.Space(15f);
+
+            GUILayout.Label("turn rate: (default 1) " + ModSettings.turnRate.ToString("N2"));
+            DrawTF(ref ModSettings.turnRate, i);
+            DrawSlider(ref ModSettings.turnRate, 0f, 100f, i);
+            i++;
+            GUILayout.Space(15f);
+
+            GUILayout.Label("bounce dampening timer: (default 3) aka airtime to disable bounciness on landing" + ModSettings.bouncetimer.ToString("N2"));
+            DrawTF(ref ModSettings.bouncetimer, i);
+            i++;
+            GUILayout.Space(55f);
+
+            if (GUILayout.Button("Sync with other players"))
+            {
+                ModSettings.SendSyncEvent();
+            }
+
+            GUILayout.EndVertical();
+
+        }
+    }
+    public class FirstPersonCharacterTokioDrift : FirstPersonCharacter
+    {
+
+
         protected override void FixedUpdate()
         {
             maximumVelocity = 100000000;
             maxVelocityChange = 100000000;
             storeMaxVelocity = 100000000;
-           
+            if (LocalPlayer.AnimControl.doShellRideMode)
+            {
+                if (UnityEngine.Input.GetKey(KeyCode.LeftShift))
+                {
+                    if (LocalPlayer.Stats.Stamina > 4)
+                    {
+                        var forceAcc = transform.forward * ModSettings.acceleration;
+                        rb.AddForce(forceAcc, ForceMode.Acceleration);
+                    }
+                    LocalPlayer.Stats.Stamina -= 3 * Time.deltaTime;
+                }
+            }
+
             base.FixedUpdate();
             maximumVelocity = 100000000;
             maxVelocityChange = 100000000;
             storeMaxVelocity = 100000000;
 
-            if (LocalPlayer.AnimControl.doShellRideMode)
-            {
-                if (UnityEngine.Input.GetKey(KeyCode.LeftShift))
-                {
-                    rb.AddForce(transform.forward *15, ForceMode.Acceleration);
-                    LocalPlayer.Stats.Stamina -= 3*Time.deltaTime;
-                }
-            }
         }
 
 
-      
+
         protected override void HandleFrictionParams()
         {
             if (LocalPlayer.AnimControl.doShellRideMode)
             {
-                if (LocalPlayer.Stats.IsInNorthColdArea())
-                {
-                    playerPhysicMaterial.staticFriction = 0.0f;
-                    playerPhysicMaterial.dynamicFriction = 0.0f;
-                }
-                else
-                {
-                    playerPhysicMaterial.staticFriction = 0.0f;
-                    playerPhysicMaterial.dynamicFriction = 0.0f;
-                }
-                if (jumpingTimer > 3f)
+
+                playerPhysicMaterial.staticFriction = ModSettings.friction;
+                playerPhysicMaterial.dynamicFriction = ModSettings.friction;
+                if (jumpingTimer > ModSettings.bouncetimer)
                 {
                     playerPhysicMaterial.bounceCombine = PhysicMaterialCombine.Minimum;
                     playerPhysicMaterial.bounciness = 0f;
@@ -55,7 +173,7 @@ namespace TheFlash
                 else
                 {
                     playerPhysicMaterial.bounceCombine = PhysicMaterialCombine.Average;
-                    playerPhysicMaterial.bounciness = 0.9f;
+                    playerPhysicMaterial.bounciness = ModSettings.bounciness;
                 }
                 playerPhysicMaterial.frictionCombine = PhysicMaterialCombine.Minimum;
             }
@@ -92,5 +210,26 @@ namespace TheFlash
                 playerPhysicMaterial.bounciness = 0f;
             }
         }
+
     }
+    public class playerAnimatorControlTokioDrift : playerAnimatorControl
+    {
+        protected override void HandleShellRideControlUpdate()
+        {
+            this.hVel = Mathf.SmoothDamp(this.hVel, TheForest.Utils.Input.GetAxis("Horizontal") * 75f, ref this.curVel, 0.1f);
+            this.shellBlendVal = Mathf.SmoothStep(this.hVel, this.shellBlendVal, Time.deltaTime / 9f);
+            this.animator.SetFloatReflected("shellBlend", this.shellBlendVal);
+            if (Mathf.Abs(this.hVel) > 0.0001f)
+            {
+                this.rootTr.RotateAround(base.transform.position, Vector3.up, this.hVel * ModSettings.turnRate * Time.deltaTime);
+            }
+            else
+            {
+                this.curVel = 0f;
+            }
+            Vector3 vector = this.tr.forward * -1f;
+        }
+    }
+
+
 }
---
+++ NetworkModule.cs
@@ -0,0 +1,119 @@
+﻿using Bolt;
+using System;
+using System.IO;
+
+namespace TokyoDrift
+{
+    public class NetworkConfig
+    {
+        public const ulong ModNetworkPacked = 999999422;
+        public readonly static NetworkId ModNetworkID = new NetworkId(ModNetworkPacked);
+        internal const string ModName = "TurtleShellMod Network Data";
+    }
+
+
+
+
+
+    //copy paste all of this below
+
+    public class NetworkCallbacksServerMod : CoopServerCallbacks
+    {
+        public override void OnEvent(ChatEvent evnt)
+        {
+            if (evnt.Sender == NetworkConfig.ModNetworkID)
+            {
+                NetworkManager.RecieveCommand(NetworkManager.StringToBytes(evnt.Message));
+                return;
+            }
+            base.OnEvent(evnt);
+        }
+    }
+    public class NetworkCallbacksClientMod : CoopPlayerCallbacks
+    {
+        public override void OnEvent(ChatEvent evnt)
+        {
+            if (evnt.Sender == NetworkConfig.ModNetworkID)
+            {
+                NetworkManager.RecieveCommand(NetworkManager.StringToBytes(evnt.Message));
+                return;
+            }
+            base.OnEvent(evnt);
+        }
+    }
+
+
+    public static class NetworkManager
+    {
+        public static byte[] StringToBytes(string cmd)
+        {
+            var a = cmd.ToCharArray();
+            var b = new byte[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                b[i] = (byte)a[i];
+            }
+            return b;
+        }
+        public static string BytesToString(byte[] b)
+        {
+            string s = string.Empty;
+            for (int i = 0; i < b.Length; i++)
+            {
+                s += (char)b[i];
+            }
+            return s;
+        }
+
+
+        /// <summary>
+        /// Sends bytes to targets, calls RecieveCommand on them
+        /// </summary>
+        /// <param name="bytearray">Bytes to send</param>
+        /// <param name="target">Choose between possible recievers</param>
+        public static void SendCommand(byte[] bytearray, GlobalTargets target)
+        {
+            if (BoltNetwork.isRunning)
+            {
+                ChatEvent chatEvent = ChatEvent.Create(target);
+                chatEvent.Message = BytesToString(bytearray);
+                chatEvent.Sender = NetworkConfig.ModNetworkID;
+                chatEvent.Send();
+            }
+        }
+
+
+        public static void RecieveCommand(byte[] b)
+        {
+            using (MemoryStream stream = new MemoryStream(b))
+            {
+                using (BinaryReader r = new BinaryReader(stream))
+                {
+                    int cmdIndex = r.ReadInt32();
+                    try
+                    {
+                        switch (cmdIndex)
+                        {
+                            case 1:
+                                ShellModOptionsGUI.Reset();
+                                ModSettings.bounciness = r.ReadSingle();
+                                ModSettings.acceleration = r.ReadSingle();
+                                ModSettings.bouncetimer = r.ReadSingle();
+                                ModSettings.friction= r.ReadSingle();
+                                ModSettings.turnRate = r.ReadSingle();
+                                break;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        ModAPI.Log.Write("Network error: " + cmdIndex + "\n\n" + e.ToString());
+                    }
+                    r.Close();
+                }
+                stream.Close();
+            }
+        }
+
+    }
+}
+
